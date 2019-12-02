a6e8a42726de44065499885bc90a54f19d4424b9
lukasjaronis <jaronisluke13@gmail.com>
2019-12-02 16:48:18 -0500

    added styles

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,69 +1,88 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-const FormStyles = styled.div `
-
-form {
- .formContainer {
-
-    input {
-        border-radius: 15px;
+const FormStyles = styled.div`
+  form {
+    .formContainer {
+      input {
         border: none;
+        background: none;
         height: 2rem;
         text-indent: 20px;
-        border: 2px solid #fff;
+        border-bottom: 2px solid #379683;
+        color: #edf5e1;
+        text-decoration: none;
+
+        &:focus {
+          outline: none;
+          border-top: 2px solid #379683;
+        }
+
+        &::placeholder {
+          color: #edf5e1;
+        }
+      }
+
+      button {
+        margin: 1rem;
+        padding: 0.4rem;
+        width: 4rem;
+        border-radius: 15px;
+        background: #379683;
+        color: #edf5e1;
+        border: none;
+
+        &:hover {
+          font-weight: 900;
+        }
+
+        &:focus {
+          outline: none;
+         
+        }
+      }
     }
-
-     button {
-         margin: 1rem;
-         padding: 0.4rem;
-         width: 4rem;
-         border-radius: 15px;
-         background: #379683;
-         color: #EDF5E1;
-         border: none;
-
-         &:hover {
-             font-weight: 900;
-         }
-     }
- }
-}
-
+  }
 `;
 
 class ToDoForm extends Component {
-constructor() {
+  constructor() {
     super();
     this.state = {
-        newToDo: ""
+      newToDo: ""
     };
-}
+  }
 
-handleChanges = element => {
-    this.setState({newToDo: element.target.value});
-};
+  handleChanges = element => {
+    this.setState({ newToDo: element.target.value });
+  };
 
-handleSubmit = element => {
+  handleSubmit = element => {
     element.preventDefault();
     this.props.addToDo(this.state.newToDo);
     this.setState({
-        newToDo: ""
+      newToDo: ""
     });
-};
+  };
 
-    render() {
-        return (
-            <FormStyles>
-            <form onSubmit={this.handleSubmit}>
-            <div className="formContainer">
-            <input placeholder="..." value={this.state.newToDo} onChange={this.handleChanges} type="text" name="todo" />
+  render() {
+    return (
+      <FormStyles>
+        <form onSubmit={this.handleSubmit}>
+          <div className="formContainer">
+            <input
+              placeholder="..."
+              value={this.state.newToDo}
+              onChange={this.handleChanges}
+              type="text"
+              name="todo"
+            />
             <button>Add</button>
-            </div>
-            </form>
-            </FormStyles>
-        );
-    }
+          </div>
+        </form>
+      </FormStyles>
+    );
+  }
 }
 
 export default ToDoForm;
